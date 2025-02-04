735b537b2c17ab4f9d16331b32150a26929f4b96
Open Source <103868788+OpenDeveloerHub@users.noreply.github.com>
2025-02-04 19:37:40 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -16,18 +16,7 @@ const app = express();
 const server = http.createServer(app);
 
 // Global CORS Middleware
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-
-    if (req.method === "OPTIONS") {
-        return res.status(200).end();
-    }
-
-    next();
-});
+app.use(cors());
 
 // Express Middleware
 app.use(express.json());
