4fb277eeaa49cd03e605fcb26ed557e7a9ca1824
Bhuvan <100340704+Bhuvantenguria@users.noreply.github.com>
2025-02-04 14:52:27 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -22,7 +22,9 @@ const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 // const io = new Server(server, { cors: { origin: '*' } });
 
-app.use(cors());
+app.use(cors(
+    origin :'*'
+));
 app.get('/', (req, res) => {
     res.send('Hellov bro');
 });
