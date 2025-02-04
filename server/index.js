3f64e3a2ec5fbe2e72ea4d953275a822ae1efdb8
Open Source <103868788+OpenDeveloerHub@users.noreply.github.com>
2025-02-04 15:12:13 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ const server = http.createServer(app);
 
 // Global CORS Middleware
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "https://focusflowed.vercel.app");
+    res.setHeader("Access-Control-Allow-Origin", "https://focusflowed.vercel.app/*");
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
     res.setHeader("Access-Control-Allow-Credentials", "true");
